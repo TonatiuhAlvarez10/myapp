e1ecdedbcb6c8ae6ff12acb32239bf09ee81a70d
tonatiuh10 <65044653+tonatiuh10@users.noreply.github.com>
2020-11-17 18:18:28 -0600

    updates to deploy to heroku

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const ruta = require ('ruta');
+
+aplicación constante = express ();
+
+// Sirve solo los archivos estáticos del directorio dist
+app.use (express.static (__ dirname + '/ dist / <my-app>'));
+
+app.get ('/ *', function (req, res) {
+    
+res.sendFile (path.join (__ dirname + '/ dist / <my-app> /index.html'));
+});
+
+// Inicie la aplicación escuchando en el puerto Heroku predeterminado
+app.listen (process.env.PORT || 8080);
